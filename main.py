677d2398cf33000abe0a0901b0df5c0a7a1ae97c
Роман <roman.v.antonnikov@gmail.com>
2025-11-29 00:13:57 +0300

    Реализовано пользовательское меню

--- main.py
+++ main.py
@@ -23,7 +23,46 @@ class ToDoList:
             print(f"{key}: {value}")
 
 
-task_list = ToDoList()
-task_list.add_task("Сделать уроки")
-task_list.complete_task("Сделать уроки")
-task_list.list_tasks()
+def show_menu():
+    print("ПОЛЬЗОВАТЕЛЬСКОЕ МЕНЮ\n")
+    print("1. Показать все задачи")
+    print("2. Добавить задачу в список")
+    print("3. Отметить задачу выполненной")
+    print("4. Удалить задачу из списка")
+    print("5. Выход")
+
+
+def main():
+    todo = ToDoList()
+
+    while True:
+        show_menu()
+        command = input("\nВыберите действие (1-5): \n").strip()
+
+        if command == "1":
+            todo.list_tasks()
+        elif command == "2":
+            task = input("Введите новую задачу:\n").strip()
+            if task:
+                todo.add_task(task)
+            else:
+                print("Нельзя добавить пустую задачу!")
+        elif command == "3":
+            todo.list_tasks()
+            if todo.task_list:
+                task = input("Введите задачу, которую хотите отметить выполненной:\n").strip()
+                todo.complete_task(task)
+        elif command == "4":
+            todo.list_tasks()
+            if todo.task_list:
+                task = input("Введите задачу, которую хотите удалить из списка:\n").strip()
+                todo.remove_task(task)
+        elif command == "5":
+            print("До свидания!")
+            break
+        else:
+            print("Неправильный выбор! Введите число от 1 до 5.")
+
+
+if __name__ == "__main__":
+    main()
