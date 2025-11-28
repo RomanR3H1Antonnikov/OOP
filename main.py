9cb35d1264229d2abe630dd7fd0e70534c9f7940
Роман <roman.v.antonnikov@gmail.com>
2025-11-28 11:16:32 +0300

    Применил некоторые правки к коду

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 class ToDoList:
     def __init__(self):
         self.task_list = {"Запустить программу" : "(СДЕЛАНО)"}
+
     def add_task(self, task):
         self.task_list[task] = "(НЕ СДЕЛАНО)"
+
     def complete_task(self, task):
         if task in self.task_list:
             self.task_list[task] = "(СДЕЛАНО)"
         else:
             print("Такой задачи нет в списке!")
+
     def remove_task(self, task):
         if task in self.task_list:
             del self.task_list[task]
         else:
             print("Такой задачи нет в списке!")
+
     def list_tasks(self):
-        print("Задачи:")
+        print("Задачи:\n")
         for key, value in self.task_list.items():
             print(f"{key}: {value}")
 
@@ -22,5 +26,4 @@ class ToDoList:
 task_list = ToDoList()
 task_list.add_task("Сделать уроки")
 task_list.complete_task("Сделать уроки")
-task_list.remove_task("Запустить программу")
 task_list.list_tasks()
