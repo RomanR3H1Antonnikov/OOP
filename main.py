d697b33acf70ad580124bb0fe3cd6fc27938d6e5
Роман <roman.v.antonnikov@gmail.com>
2025-11-29 11:56:13 +0300

    Обновил код, применив все правки. Теперь код готов к теоритическому расширению функционала

--- main.py
+++ main.py
@@ -4,64 +4,72 @@ class ToDoList:
 
     def add_task(self, task):
         self.task_list[task] = "(НЕ СДЕЛАНО)"
+        print(f"Задача {task} успешно добавлена!")
 
     def complete_task(self, task):
         if task in self.task_list:
             self.task_list[task] = "(СДЕЛАНО)"
+            print(f"Задача {task} успешно отмечена как выполненная!")
         else:
             print("Такой задачи нет в списке!")
 
     def remove_task(self, task):
         if task in self.task_list:
             del self.task_list[task]
+            print(f"Задача {task} успешно удалена из списка")
         else:
             print("Такой задачи нет в списке!")
 
     def list_tasks(self):
-        print("Задачи:\n")
+        print("Задачи:")
         for key, value in self.task_list.items():
             print(f"{key}: {value}")
+        print("\n")
 
 
-def show_menu():
-    print("ПОЛЬЗОВАТЕЛЬСКОЕ МЕНЮ\n")
-    print("1. Показать все задачи")
-    print("2. Добавить задачу в список")
-    print("3. Отметить задачу выполненной")
-    print("4. Удалить задачу из списка")
-    print("5. Выход")
+def get_valid_input(inpt):
+    while True:
+        value = input(inpt).strip()
+        if value:
+            return value
+        print("Название задачи не может быть пустым!")
+
+
+def print_menu(menu_data):
+    print("Доступные функции:")
+    for key in sorted(menu_data.keys()):
+        text, _ = menu_data[key]
+        print(f"{key}. {text}")
+    print("Введите номер функции: (1 - 5)")
 
 
 def main():
     todo = ToDoList()
-
+    text_to_write = "Введите название задачи:"
+    menu_data = {
+        1:("Показать все задачи", lambda: todo.list_tasks()),
+        2:("Добавить задачу в список", lambda: todo.add_task(get_valid_input(text_to_write))),
+        3:("Отметить задачу выполненной", lambda: todo.complete_task(get_valid_input(text_to_write))),
+        4:("Удалить задачу из списка", lambda: todo.remove_task(get_valid_input(text_to_write))),
+        5:("Выход", None)
+    }
     while True:
-        show_menu()
-        command = input("\nВыберите действие (1-5): \n").strip()
+        try:
+            print("ПОЛЬЗОВАТЕЛЬСКОЕ МЕНЮ")
+            print_menu(menu_data)
+            command = int(input())
 
-        if command == "1":
-            todo.list_tasks()
-        elif command == "2":
-            task = input("Введите новую задачу:\n").strip()
-            if task:
-                todo.add_task(task)
+            if command == 5:
+                print("До свидания!")
+                break
+            elif command in menu_data:
+                _, action = menu_data[command]
+                if action:
+                    action()
             else:
-                print("Нельзя добавить пустую задачу!")
-        elif command == "3":
-            todo.list_tasks()
-            if todo.task_list:
-                task = input("Введите задачу, которую хотите отметить выполненной:\n").strip()
-                todo.complete_task(task)
-        elif command == "4":
-            todo.list_tasks()
-            if todo.task_list:
-                task = input("Введите задачу, которую хотите удалить из списка:\n").strip()
-                todo.remove_task(task)
-        elif command == "5":
-            print("До свидания!")
-            break
-        else:
-            print("Неправильный выбор! Введите число от 1 до 5.")
+                print("Неверная команда!")
+        except ValueError:
+            print("Команда не введена!")
 
 
 if __name__ == "__main__":
